852551c35ed654d0d036b3bc059bedec1933c649
Abhiraj Bhatia <68340625+1810991483@users.noreply.github.com>
2020-07-19 10:43:52 +0530

    Update DuplicateElement.java

--- DuplicateElement.java
+++ DuplicateElement.java
@@ -1,10 +1,10 @@
-public class Main
+import java.util.*;
+class DuplicateElement
 {
-
     public static void main(String[] args) 
     {
         int[] a = {2, 6, 7, 6, 2, 19, 1, 19};
-        Set<Integer> numSet = new HashSet<Integer>();
+        HashSet<Integer> numSet = new HashSet<Integer>();
         for(int num : a){
             
             if(!numSet.add(num)){
