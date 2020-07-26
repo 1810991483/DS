7dfe8fc5c4bab0c0a792067b788707f2669ed194
Abhiraj Bhatia <68340625+1810991483@users.noreply.github.com>
2020-07-26 10:37:40 +0530

    Update DuplicateElement.java

--- DuplicateElement.java
+++ DuplicateElement.java
@@ -3,7 +3,15 @@ class DuplicateElement
 {
     public static void main(String[] args) 
     {
-        int[] a = {2, 6, 7, 6, 2, 19, 1, 19};
+        int n;
+       // int[] a = {2, 6, 7, 6, 2, 19, 1, 19};
+        Scanner s=new Scanner(System.in);
+        n=s.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+        }
         HashSet<Integer> numSet = new HashSet<Integer>();
         for(int num : a){
             
