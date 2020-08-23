b6dcbb43cf4eb6b5872d9c63ef4f7e0055a3ddeb
Abhiraj Bhatia <68340625+1810991483@users.noreply.github.com>
2020-08-23 11:14:17 +0530

    Create infixtopostix.java

---
+++ infixtopostix.java
@@ -0,0 +1,85 @@
+import java.util.*; 
+  
+class Main
+{ 
+    
+    static int P(char ch) 
+    { 
+        switch (ch) 
+        { 
+        case '+': 
+        case '-': 
+            return 1; 
+       
+        case '*': 
+        case '/': 
+            return 2; 
+       
+        case '^': 
+            return 3; 
+        } 
+        return -1; 
+    } 
+       
+   
+    static String ToPostfix(String exp) 
+    { 
+        
+        String r = new String(""); 
+          
+        
+        Stack<Character> stack = new Stack<>(); 
+          
+        for (int i = 0; i<exp.length(); ++i) 
+        { 
+            char c = exp.charAt(i); 
+              
+            
+            if (Character.isLetterOrDigit(c)) 
+                r += c; 
+               
+           
+            else if (c == '(') 
+                stack.push(c); 
+              
+            
+            else if (c == ')') 
+            { 
+                while (!stack.isEmpty() && stack.peek() != '(') 
+                    r += stack.pop(); 
+                  
+                if (!stack.isEmpty() && stack.peek() != '(') 
+                    return "Invalid";              
+                else
+                    stack.pop(); 
+            } 
+            else 
+            { 
+                while (!stack.isEmpty() && P(c) <= P(stack.peek())){ 
+                    if(stack.peek() == '(') 
+                        return "Invalid "; 
+                    r += stack.pop(); 
+             } 
+                stack.push(c); 
+            } 
+       
+        } 
+       
+       
+        while (!stack.isEmpty()){ 
+            if(stack.peek() == '(') 
+                return "Invalid"; 
+            r += stack.pop(); 
+         } 
+        return r; 
+    } 
+    
+    
+    public static void main(String[] args)  
+    { 
+       Scanner s1=new Scanner(System.in);
+		String ch=new String();
+		ch=s1.nextLine();
+		System.out.println(ToPostfix(ch));
+    } 
+} 
